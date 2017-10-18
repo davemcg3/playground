72d36fe3d6c9c5b7e6a0bc89f4835b6fdab969ba
davemcg3 <dave.mcgrath3@gmail.com>
2017-10-18 02:10:24 -0700

    Working on adding new cards when FAB clicked

--- react/pinger/src/client/app/Configurator.jsx
+++ react/pinger/src/client/app/Configurator.jsx
@@ -1,36 +1,81 @@
 import React from 'react';
 import {Button} from 'react-toolbox/lib/button';
+import Dialog from 'react-toolbox/lib/dialog';
+import Input from 'react-toolbox/lib/input';
 
 class Configurator extends React.Component {
 
   constructor(props) {
     super(props);
+    //props['siteName'] could be undefined which would make it an 'uncontrolled' input, the || '' makes it 'controlled' by default, avoiding the console warning
     this.state = {
+      siteName: props['siteName'] || '',
+      siteUrl: props['siteUrl'] || '',
+      method: props['method'] || '',
+      payload: props['payload'] || '',
       active: false
     };
+    // This binding is necessary to make `this` work in the callback
+    this.handleToggle = this.handleToggle.bind(this);
+    this.saveOptions = this.saveOptions.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.loadOptions = this.loadOptions.bind(this);
   }
 
-  handleToggle = () => {
+  loadOptions (options) {
+    console.log(this.state);
+    console.log(options);
+    for (var key in options) {
+      var temp = { [key]: options[key]};
+      console.log(temp);
+      this.setState(temp);
+    }
+    console.log(this.state);
+    console.log(this.state.siteName);
+    console.log(this.state);
+  }
+
+  handleToggle () {
     this.setState({active: !this.state.active});
+    //this.loadOptions({siteName: 'booga '})
   }
 
-  actions = [
-    { label: "Cancel", onClick: this.handleToggle },
-    { label: "Save", onClick: this.handleToggle }
-  ];
+  handleChange (name, value) {
+    this.setState({ [name]: value });
+  };
+
+  saveOptions () {
+    console.log(this.state);
+    //emit this state up the chain so it can be handled by the PingerCard
+    this.setState({
+      active: !this.state.active
+    });
+  }
 
   render() {
+    const actions = [
+      { label: "Cancel", onClick: this.handleToggle },
+      { label: "Save", onClick: this.saveOptions }
+    ];
+
     return (
       <div>
         <Button label='Show my dialog' onClick={this.handleToggle} />
         <Dialog
-          actions={this.actions}
+          actions={actions}
           active={this.state.active}
           onEscKeyDown={this.handleToggle}
           onOverlayClick={this.handleToggle}
-          title='My awesome dialog'
+          title='Configurator'
         >
-          <p>Here you can add arbitrary content. Components like Pickers are using dialogs now.</p>
+          <div>
+            <section>
+              <Input type='text' label='site name' name='siteName' value={this.state.siteName} onChange={this.handleChange.bind(this, 'siteName')} />
+              <Input type='text' label='site url' name='siteUrl' value={this.state.siteUrl} onChange={this.handleChange.bind(this, 'siteUrl')} />
+              <Input type='text' label='method' name='method' value={this.state.method} onChange={this.handleChange.bind(this, 'method')} />
+              <Input type='text' label='payload' name='payload' value={this.state.payload} onChange={this.handleChange.bind(this, 'payload')} />
+            </section>
+          </div>
         </Dialog>
       </div>
     );
--- react/pinger/src/client/app/Fab.jsx
+++ react/pinger/src/client/app/Fab.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Button} from 'react-toolbox/lib/button';
+import Configurator from './Configurator.jsx'
 
 class Fab extends React.Component {
 
@@ -7,16 +8,18 @@ class Fab extends React.Component {
     super(props);
     this.state = {
     };
-    this.checkSite = this.checkSite.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  checkSite () {
+  handleClick () {
+    this.configurator.loadOptions({active:true})
   }
 
   render() {
     return (
       <div>
-        <Button id='fab' icon='add' floating accent />
+        <Button id='fab' icon='add' floating accent onClick={this.handleClick} />
+        <Configurator ref={(conf) => { this.configurator = conf; }} />
       </div>
     );
   }
